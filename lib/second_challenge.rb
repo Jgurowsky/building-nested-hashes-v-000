9c8c420a27770769d3e5e310ddb3a4002ad0334f
jgurowsky@gmail.com <jgurowsky@gmail.com>
2018-07-14 19:05:33 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,11 +1,8 @@
 def second_challenge
   epic_tragedy = {
-    :montague => {
-      #your key/value pairs here!
-      }, 
-    :capulet => {
-      #your key/value pairs here!
-      }}
+    :montague => {},
+    :capulet => {}
+  }
 
-  
+
 end
